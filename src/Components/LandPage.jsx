7eb60294f5878e219323b93cc8d05434d5e1ffe4
Varsha <singhpratibha66420@gmail.com>
2026-02-03 21:41:43 +0530

    Add Landing Page Layout

--- src/Components/LandPage.jsx
+++ src/Components/LandPage.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const LandPage = () => {
   return (
-    <div>LandPage</div>
+    <div className='w-full h-screen bg-gradient-to-b from-[#C7B8F3] via-[#E6EEFF] to-[#FCFCFF]'>
+        <div className='flex justify-center items-center w-full h-screen border-2'>
+            <h4>A calm place to think</h4>
+            <h1>Paperlane</h1>
+            <h3>From fleeting sparks to polished plans, Paperlane turns every note into a journey worth keeping.</h3>
+            <button>Add Note</button>
+        </div>
+    </div>
   )
 }
 
