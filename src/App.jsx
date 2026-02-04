0de52206034ed2160d474aca1c6d07cebb37c7ed
Varsha <singhpratibha66420@gmail.com>
2026-02-04 20:18:00 +0530

    Add the style and font to the landingPage

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import LandPage from "./Components/LandPage";
 function App() {
 
   return (
-    <>
+    <div style={{fontFamily: "Inter"}}>
      <LandPage/>
-    </>
+    </div>
   )
 }
 
--- src/Components/LandPage.jsx
+++ src/Components/LandPage.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const LandPage = () => {
   return (
-    <div className='w-full h-screen bg-gradient-to-b from-[#C7B8F3] via-[#E6EEFF] to-[#FCFCFF]'>
-        <div className='flex justify-center items-center w-full h-screen border-2'>
-            <h4>A calm place to think</h4>
-            <h1>Paperlane</h1>
-            <h3>From fleeting sparks to polished plans, Paperlane turns every note into a journey worth keeping.</h3>
-            <button>Add Note</button>
+    <div className='w-full h-screen flex justify-center items-center bg-gradient-to-b from-[#C7B8F3] via-[#E6EEFF] to-[#FCFCFF]'>
+        <div className='flex flex-col justify-center items-center '>
+            <h4 className='text-[#5A648C]'>A calm place to think</h4>
+            <h1 style={{fontFamily: "Pacifico"}} className='text-[#1B2559]'>Paperlane</h1>
+            <h3 className='text-[#6B7396]'>From fleeting sparks to polished plans,<br/> Paperlane turns every note into a journey worth keeping.</h3>
+            <button className='text-[#ffffff] bg-[#5E8BFF] p-[10px] rounded-[50px] w-[150px]'>Add Note</button>
         </div>
     </div>
   )
