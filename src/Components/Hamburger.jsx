c27ab3bd5a53ee05c6f9f1a0d83aa69c73a21d28
Varsha <singhpratibha66420@gmail.com>
2026-03-08 19:02:48 +0530

    add the data tab and danger zone tab in the setting

--- src/Components/Hamburger.jsx
+++ src/Components/Hamburger.jsx
@@ -8,9 +8,27 @@ const Hamburger = ({menuClose, clearAllHandler, sortNote, filterNotes, resetfilt
   const [settingPopUp, setSettingPopUp] = useState(false); //open the setting
   const [prefeOpen, setPrefeOpen] = useState(false); //open the Preference option inside the settings
   const [themeOpen, setThemeOpen] = useState(false); //open the theme option inside the preference
-  const [selectedTheme, setSelectedTheme] = useState("Light");
+  const [selectedTheme, setSelectedTheme] = useState("Light"); //option for the theme preferences
+  const [SizeOpen, setSizeOpen] = useState(false); //open the theme option inside the preference
+  const [selectedSize, setSelectedSize] = useState("Medium"); //option for the theme preferences
+  const [dataTabOpen, setDataTabOpen] = useState(false); //open the data tab in the setting
+  const [noteCount, setNoteCount] = useState("0"); //state for the note Count
+  const [mbUsed, setMbUsed] = useState("0"); //state for the mb used by the note
+  const [dangerTabOpen, setDangerTabOpen] = useState(false); //open the danger tab of the setting
+  const [resetPopUp, setResetPopUp] = useState(false); //this popup is open when the user click on the reset button
 
   const menuRef = useRef(null);
+
+  // This function will count the total notes in the memory and also how many memory the notes used 
+  function storageInfo(){
+    const notes = JSON.parse(localStorage.getItem("paperlane_notes")) || [];
+    const notesCount = notes.length;
+
+    const notesString = JSON.stringify(notes);
+    const bytes = new Blob([notesString]).size;
+    const mbUsed = (bytes / (1024 * 1024)).toFixed(2);
+    return [notesCount, mbUsed];
+  }
 
   // when the user click outside the menu it will close the menu
   useEffect(() => {
@@ -19,7 +37,10 @@ const Hamburger = ({menuClose, clearAllHandler, sortNote, filterNotes, resetfilt
         menuClose();
       }
     }
-    
+    let arr = storageInfo();
+    setNoteCount(arr[0]);
+    setMbUsed(arr[1]);
+
     document.addEventListener("mousedown", handleClickOutside);
 
     return () => {
@@ -45,15 +66,16 @@ const Hamburger = ({menuClose, clearAllHandler, sortNote, filterNotes, resetfilt
                 <h4 className="text-white font-semibold text-[22px] mb-4">
                   Settings
                 </h4>
+
+                {/* Preference tab of the setting */}
                 <div className="flex flex-col bg-gray-400 rounded-md mb-2 py-3">
-                  <div
-                    className="flex w-full items-center justify-between px-4"
-                    onClick={() => setPrefeOpen((prev) => !prev)}
-                  >
+                  <div className="flex w-full items-center justify-between px-4"
+                    onClick={() => setPrefeOpen((prev) => !prev)} >
                     <p>Preferences</p>
-                    <img src="/icons/dropdown.svg" alt="" />
+                    <img src="/icons/dropdown.svg" alt="dropdown of preference tab" />
                   </div>
-                  {/* Sub options of the prefernce */}
+
+                  {/* Sub options of the prefernce Tab */}
                   {prefeOpen && (
                     <>
                     {/* Theme Row */}
@@ -88,35 +110,89 @@ const Hamburger = ({menuClose, clearAllHandler, sortNote, filterNotes, resetfilt
                         </div>
                       </div>
 
-                          {/* FontSize row */}
-                          <hr className="my-3 border-white/20 w-full"/>
-                          <div className="flex items-center justify-between px-4">
-                            <p>Font Size</p>
-                            <div className="flex flex-col w-[120px] bg-gray-500/75 rounded-md px-2 py-1 cursor-pointer">
+                      {/* FontSize row */}
+                      <hr className="my-3 border-white/20 w-full"/>
+                      <div className="flex items-center justify-between px-4">
+                        <p>Font Size</p>
+                      <div className="flex flex-col w-[120px] bg-gray-500/75 rounded-md px-2 py-1 cursor-pointer">
 
-                            {/* Font Size option show */}
-                          <div className="flex items-center justify-between"
-                            onClick={() => setThemeOpen((prev) => !prev)}>
-                            <p className="text-white">{selectedTheme}</p>
-                            <img src="/icons/dropdown.svg" alt="" />
-                          </div>
+                      {/* Font Size option show */}
+                      <div className="flex items-center justify-between" onClick={() => setSizeOpen((prev) => !prev)}>
+                        <p className="text-white">{selectedSize}</p>
+                        <img src="/icons/dropdown.svg" alt="dropdown of the font size option" />
+                      </div>
 
+                      {/* Dropdown options inside the FontSize */}
+                      {SizeOpen && (
+                      <ul className="mt-2 bg-gray-600 rounded-md">
+                        {["Small", "Medium",  "Large"].map((option) => (
+                         <li key={option}
+                          className={`px-2 py-1 rounded cursor-pointer  ${ selectedSize === option ? "bg-blue-100 text-blue-600 font-semibold"  : "hover:bg-gray-500 text-white"  }`}
+                          onClick={() => {
+                          setSelectedSize(option); // update selection
+                          setSizeOpen(false); // close dropdown
+                          }}>
+                            {option}
+                          </li>
+                          ))}
+                        </ul>
+                        )}
                             </div>
-                          </div>
+                      </div>
                     </>
                   )}
                 </div>
+
+                {/* Data Tab of the setting */}
+                <div className="flex flex-col bg-gray-400 rounded-md mb-2 py-3">
+                  <div className="flex w-full items-center justify-between px-4"
+                    onClick={() => setDataTabOpen(prev => !prev)}>
+                    <p> Data </p>
+                    <img src="/icons/dropdown.svg" alt="dropdown of data tab" />
+                  </div>
+
+                {/* Sub option of the dataTab */}
+                {dataTabOpen && (
+                  <>
+                  {/* Storage info row */}
+                  <hr className="my-3 border-white/20 w-full" />
+                  <div className="px-4">
+                    <p>Storage Info</p>
+                    <div className='flex flex-col mt-2'>
+                      <p>Notes stored: {noteCount} </p>
+                      <p>Storage used: {mbUsed} MB </p>
+                    </div>
+                  </div>
+                  </>
+                )}
+                </div>
+
+                {/* Danger Tab of the setting */}
+                <div className="flex flex-col bg-gray-400 rounded-md mb-2 py-3">
+                  <div className="flex w-full items-center justify-between px-4"
+                    onClick={() => setDangerTabOpen(prev => !prev)}>
+                    <p> Danger Zone </p>
+                    <img src="/icons/dropdown.svg" alt="dropdown of danger zone tab" />
+                  </div>
+
+                {/* Sub option of the dataTab */}
+                {dangerTabOpen && (
+                  <>
+                  {/* Storage info row */}
+                  <hr className="my-3 border-white/20 w-full" />
+                  <div className="px-4" onClick={() => {setSettingPopUp(prev => !prev); setResetPopUp(true)}}>
+                    <p className='text-red-500'>Reset App</p>              
+                  </div>
+                  </>
+                )}
+                </div>
+
+                {/* Button of the setting */}
                 <div className="flex justify-end gap-2 mt-4">
-                  <button
-                    className="bg-blue-500 text-white px-3 py-1 rounded"
-                    onClick={() => setSettingPopUp(false)}
-                  >
+                  <button className="bg-blue-500 text-white px-3 py-1 rounded" onClick={() => setSettingPopUp(false)}>
                     Save
                   </button>
-                  <button
-                    className="bg-gray-500 text-white px-3 py-1 rounded"
-                    onClick={() => setSettingPopUp(false)}
-                  >
+                  <button className="bg-gray-500 text-white px-3 py-1 rounded" onClick={() => setSettingPopUp(false)}>
                     Cancel
                   </button>
                 </div>
@@ -234,6 +310,19 @@ const Hamburger = ({menuClose, clearAllHandler, sortNote, filterNotes, resetfilt
           )}
         </ul>
       </div>
+      {/* reset popup */}
+      {resetPopUp && (
+      <div className='fixed inset-0 flex justify-center items-center z-50'>
+        <div className='bg-gray-300 rounded-md w-[450px] h-auto p-4 shadow-md border border-black/25 text-gray-800'>
+          <h1 className='text-rose-600 font-semibold text-[20px]'>Reset App</h1>
+          <h4 className='mt-4 mb-6 text-center text-[18px]'>This will delete all notes and restore the app to its default state. This action cannot be undone.</h4>
+          <div className='flex items-center justify-between px-2'>
+            <button className='bg-red-500 rounded p-1 text-white border border-white'>Reset App</button>
+            <button className=''>Cancel</button>
+          </div>
+        </div>
+      </div>
+      )}
     </div>
   );
 }
