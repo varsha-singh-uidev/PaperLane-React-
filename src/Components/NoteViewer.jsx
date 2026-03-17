0f5a55602d056b396c88cd42f40453d743021d65
Varsha <singhpratibha66420@gmail.com>
2026-03-17 20:45:48 +0530

    Complete the basic structure of the noteViewer not the logic

--- src/Components/NoteViewer.jsx
+++ src/Components/NoteViewer.jsx
@@ -39,14 +39,39 @@ const NoteViewer = () => {
 
       <hr className={`${theme === "Light" ? "border-gray-300" : "border-[#2A2A3B]"}`} />
 
-      {/* main content of the page */}
+      {/* show the date of the page */}
       <div className='p-5'>
         <div className={`flex justify-center gap-2 ${theme === "Light" ? "text-black/25" : "text-gray-200/25"}`}>
-          <p>{new Date (note.updatedAt).toLocaleDateString()}</p>
+          <p>{new Date (note.updatedAt).toLocaleDateString("en-US",{
+                month : "long",
+                day : "numeric",
+                year : "numeric"
+              })}
+          </p>
           <p>|</p>
           <p>Saved "logic"</p>
         </div>
       </div>
+
+      {/* main note content */}
+      <div className="flex-1 w-full px-6 md:px-20 py-4 pb-28 overflow-y-auto">
+        <div
+        contentEditable
+        suppressContentEditableWarning={true}
+        className={`outline-none w-full min-h-[50vh] text-[16px] leading-7
+        ${theme === "Light" ? "bg-white text-black" : "bg-[#12121A] text-gray-200"}`}
+        placeholder="Start writing..."
+        >
+        </div>
+      </div>
+
+      {/* action bar */}
+      <div className={`fixed bottom-5 left-1/2 -translate-x-1/2 w-[80%] md:w-[850px] z-50`}>
+        <div className={`rounded-xl p-4 flex items-center justify-between ${theme === "Light" ? "bg-gray-200" : "bg-[#2A2A3B]"}`}>
+
+        </div>
+      </div>
+
 
     </div>
   );
